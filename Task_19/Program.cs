288d7c6e26c980c253170d3930cec84478c6fb9f
Pantokha Alexander <ealex47@gmail.com>
2022-12-12 18:16:35 +0300

    Сделал первое задание

---
+++ Task_19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? num = Console.ReadLine();
+
+void CheckingNumber(string num){
+  if (num[0]==num[4] || num[1]==num[3]){
+    Console.WriteLine($"Ваше число: {num} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+}
+
+if (num!.Length == 5){
+  CheckingNumber(num);
+}
+else Console.WriteLine($"Введи правильное число");
